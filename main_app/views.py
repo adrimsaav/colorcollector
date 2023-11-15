51f885f3cde4e3b503553378a65d601a80f214da
adrimsaav <adrimsaav@gmail.com>
2023-11-15 10:46:21 -0500

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+colors = [
+  {'name': 'Viva Magenta', 'description': 'purplish-red', 'year': 2023},
+  {'name': 'Very Peri', 'description': 'warm periwinkle, a bold lavender', 'year': 2022},
+  {'name': 'Illuminating', 'description': 'bright, cheerful yellow', 'year': 2021},
+  {'name': 'Ultimate Gray', 'description': 'timeless stone gray', 'year': 2021},
+  {'name': 'Classic Blue', 'description': 'a rich, mid-tone blue', 'year': 2020},
+  {'name': 'Living Coral', 'description': 'pinkish red-orange', 'year': 2019},
+]
