ff1203d5f4a62d601c1f06913bb6c0753ccb66b7
adrimsaav <adrimsaav@gmail.com>
2023-11-15 23:24:44 -0500

    part2

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('colors/', views.colors_index, name='index')
+  path('colors/', views.colors_index, name='index'),
+  path('colors/<int:color_id>/', views.colors_detail, name='detail')
 ]
