df311c1390da236bf9a44ccdbe0570c80a704a39
adrimsaav <adrimsaav@gmail.com>
2023-11-17 13:20:36 -0500

    part3

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('colors/', views.colors_index, name='index'),
   path('colors/<int:color_id>/', views.colors_detail, name='detail'),
-  path('colors/create/', views.ColorCreate.as_view(), name='colors_create')
+  path('colors/create/', views.ColorCreate.as_view(), name='colors_create'),
+  path('colors/<int:pk>/update/', views.ColorUpdate.as_view(), name='colors_update'),
+  path('colors/<int:pk>/delete/', views.ColorDelete.as_view(), name='colors_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Color
 
 # Create your views here.
@@ -35,3 +35,11 @@ def colors_detail(request, color_id):
 class ColorCreate(CreateView):
   model = Color
   fields = '__all__'
+
+class ColorUpdate(UpdateView):
+  model = Color
+  fields = ['description', 'year']
+
+class ColorDelete(DeleteView):
+  model = Color
+  success_url = "/colors"
